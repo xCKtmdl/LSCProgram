e1b3c944cc87ed93a20a925c43f75a6840207b82
xCKtmdl <aarongab@gmail.com>
2022-11-04 22:06:42 -0400

    add code.

---
+++ LSCProgram/Models/Models.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LSC.Models
+{
+    public class Answer
+    {
+        [JsonPropertyName("subscribers")]
+        public List<string> Subscribers { get; set; }
+    }
+    public class ApiResponse
+    {
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+    }
+    public class ApiResponseMagazine : ApiResponse
+    {
+        [JsonPropertyName("data")]
+        public List<Magazine> Data { get; set; }
+    }
+    public class Magazine
+    {
+        [JsonPropertyName("id")]
+        public Int32 Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("category")]
+        public string Category { get; }
+    }
+    public class ApiResponseSubscriber : ApiResponse
+    {
+        [JsonPropertyName("data")]
+        public List<Subscriber> Data { get; set; }
+    }
+    public class Subscriber
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("magazineIds")]
+        public List<int> MagazineIds { get; set; }
+    }
+    public class ApiResponseAnswerResponse : ApiResponse
+    {
+        [JsonPropertyName("data")]
+        public AnswerResponse Data { get; set; }
+    }
+    public class AnswerResponse
+    {
+        [JsonPropertyName("totalTime")]
+        public string TotalTime { get; set; }
+
+        [JsonPropertyName("answerCorrect")]
+        public bool AnswerCorrect { get; set; }
+
+        [JsonPropertyName("shouldBe")]
+        public List<string> ShouldBe { get; set; }
+    }
+    public class ApiResponseString : ApiResponse
+    {
+        [JsonPropertyName("data")]
+        public List<string> Data { get; set; }
+    }
+
+}
+
---
+++ LSCProgram/Program.cs
@@ -0,0 +1,74 @@
+﻿using LSC.Models;
+using LSCProgram.RESTClient;
+using LSCProgram.Services;
+using Newtonsoft.Json.Linq;
+
+/*
+ * Aaron Gabriel
+ * Life Science Project
+ * note: I am aware there is no error checking in here.
+ * I did the bare minimum to get the results and extra points.
+ */
+
+namespace LSC
+{
+    public class LSCProgram
+    {
+        private static readonly string token;
+        private static Client _restClient;
+        private static readonly List<List<int>> categoryMagazines = new List<List<int>>();
+        public static async Task Main(string[] args)
+        {
+            Initialization();
+            LSCService lscService = new LSCService();
+            var token = await lscService.GetToken();
+            var categories = await lscService.GetCategories(token);
+
+            // This part is independent. This is where to get the extra points.
+            // You can add all these get magazines per category into a Task,
+            // and then run them in parallel.
+            var getMagazinesTask = new List<Task>();
+            foreach (string category in categories)
+            {
+                getMagazinesTask.Add(Task.Run(async () =>
+                {
+                    var magazines = await lscService.GetMagazines(token, category);
+                    categoryMagazines.Add(magazines);
+                }));
+            }
+            await Task.WhenAll(getMagazinesTask);
+
+            List<string> subscribedToAllCategories = new List<string>();
+            var subscribers = await lscService.GetSubscribers(token);
+
+            foreach (Subscriber subscriber in subscribers)
+            {
+                bool isSubscribedToAllCategories = true;
+
+                foreach (var magazines in categoryMagazines)
+                {
+                    if (!magazines.Any(x => subscriber.MagazineIds.Contains(x)))
+                    {
+                        isSubscribedToAllCategories = false;
+                        break;
+                    }
+                }
+
+                if (isSubscribedToAllCategories)
+                {
+                    subscribedToAllCategories.Add(subscriber.Id);
+                }
+            }
+
+            var result = await lscService.PostAnswer(token, subscribedToAllCategories);
+        }
+
+        public static void Initialization()
+        {
+            JObject appSettings = JObject.Parse(File.ReadAllText("appsettings.json"));
+            _restClient = new Client(appSettings["APIUrl"].ToString());
+        }
+    }
+}
+
+
