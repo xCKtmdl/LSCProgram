a3c530a0906a67410a7a4e7f41efc57fdc7d36dc
xCKtmdl <aarongab@gmail.com>
2022-11-04 22:12:16 -0400

    rest of code and exe

---
+++ LSCProgram/RESTClient/RESTClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Headers;
+
+namespace LSCProgram.RESTClient
+{
+    public class Client
+    {
+        public static HttpClient httpClient { get; set; }
+        public Client(string baseUrl)
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(baseUrl);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ LSCProgram/Services/LSCService.cs
@@ -0,0 +1,66 @@
+﻿using LSCProgram.RESTClient;
+using LSC.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace LSCProgram.Services
+{
+    public class LSCService
+    {
+        public async Task<string> GetToken()
+        {
+            using (HttpResponseMessage response = await Client.httpClient.GetAsync("api/token"))
+            {
+                var apiResponse = await response.Content.ReadAsAsync<ApiResponse>();
+                return apiResponse.Token;
+                    
+            }
+        }
+        public async Task<List<string>> GetCategories(string token)
+        {
+            using (HttpResponseMessage response = await Client.httpClient.GetAsync($"api/categories/{token}"))
+            {
+                var apiResponse = await response.Content.ReadAsAsync<ApiResponseString>();
+                return apiResponse.Data;
+            }
+        }
+        public async Task<List<int>> GetMagazines(string token, string category)
+        {
+            using (HttpResponseMessage response = await Client.httpClient.GetAsync($"api/magazines/{token}/{category}/"))
+            {
+                var apiResponse = await response.Content.ReadAsAsync<ApiResponseMagazine>();
+                List<int> magazines = new List<int>();
+                foreach (Magazine magazine in apiResponse.Data)
+                {
+                    magazines.Add(magazine.Id);
+                }
+                return magazines;
+            }
+        }
+        public async Task<List<Subscriber>> GetSubscribers(string token)
+        {
+            using (HttpResponseMessage response = await Client.httpClient.GetAsync($"api/subscribers/{token}"))
+            {
+                var apiResponse = await response.Content.ReadAsAsync<ApiResponseSubscriber>();
+                return apiResponse.Data;
+            }
+        }
+        public async Task<AnswerResponse> PostAnswer(string token, List<string> content)
+        {
+            var contentObject = new Dictionary<string, List<string>>
+            {
+                {
+                    "subscribers", content
+                },
+            };
+
+            string contentObjectSerialized = JsonConvert.SerializeObject(contentObject);
+            using (HttpResponseMessage response = await Client.httpClient.PostAsync($"api/answer/{token}", new StringContent(contentObjectSerialized, Encoding.UTF8, "application/json")))
+            {
+                var apiResponse = await response.Content.ReadAsAsync<ApiResponseAnswerResponse>();
+                return apiResponse.Data;
+            }
+        }
+    }
+}
